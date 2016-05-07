31c311df331185dbcadedd22c4f3ae500a707f0f
Leyanor <zymbiont@hotmail.ch>
2016-05-07 17:17:03 +0200

    Update random_quote.pbot.js

--- random_quote.pbot.js
+++ random_quote.pbot.js
@@ -24,6 +24,7 @@ var quotes = [
     "Me: Have a nice day, sir.  *guy leaves store, gets hit by car crossing street*  *I run out, kneel beside him*  What did I just fucken tell you!?",
     "Kids have you applied the minty paste to the exposed part of your skeleton? Yes? Well now its time to lie down in a dark room for hours.",
     "„So doc... am I dying?“ „We're all dying, just at different paces“ „But what about me?“ „You're like, the Usain Bold of dying dude lmao“",
+    "Imagine carrying a baby for 9 months and going through the pain of childbirth just to catch it masturbating to pony porn 15 years later"
     
 ]
 
